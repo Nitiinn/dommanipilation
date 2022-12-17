c718b4de4a6d1a3d812973067377053dcef52b85
Nitiinn <nk668209@gmail.com>
2022-12-17 12:50:48 +0530

    first commit

---
+++ dom3/dom3.js
@@ -0,0 +1,19 @@
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.url);
+// console.log(document.title);
+// // document.title=123;
+// console.log(document.doctype);
+
+var headertitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+// console.log(headertitle);
+// headertitle.textContent = 'hello';
+// headertitle.innerText = 'goodbye';
+header.style.borderBottom = 'solid 3px #000';
+var additemsbold = document.getElementsByClassName('title');
+additemsbold[0].style.fontWeight = 'bold';
+additemsbold[0].style.color = '#008000';
+console.log(additemsbold);
+
+
