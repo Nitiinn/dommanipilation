8f829ef587f59c12172e1ffc0cacaaa7b4a8edcd
Nitiinn <nk668209@gmail.com>
2023-02-03 21:22:24 +0530

    kickstartnodejs(1)

---
+++ first-app.js
@@ -0,0 +1,5 @@
+// programe to print hello world on screen 
+console.log('hello world from node.js');
+// program to create a new file using js
+const fs = require('fs');
+fs.writeFileSync('hello.txt','Hello form node.js');
