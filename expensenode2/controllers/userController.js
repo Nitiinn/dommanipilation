b6a9b44461549b9824223fee22351c924a3e2fae
Nitiinn <nk668209@gmail.com>
2023-02-21 17:15:50 +0530

    expense node2

---
+++ expensenode2/controllers/userController.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const Sequelize = require("sequelize");
+const User = require("../models/userModel");
+
+exports.getappPage = (req, res, next) => {
+  res.sendFile(path.join(__dirname, "../", "public", "views", "index.html"));
+};
+
+exports.getloginPage = (req, res, next) => {
+  res.sendFile(path.join(__dirname, "../", "public", "views", "signup.html"));
+};
+exports.getUsers = (req, res, next) => {
+  User.findAll()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.addExpense = (req, res, next) => {
+  const description = req.body.description;
+  const catagory = req.body.catagory;
+  const amount = req.body.amount;
+
+  User.create({
+    description: description,
+    catagory: catagory,
+    amount: amount,
+  })
+    .then((result) => {
+      console.log("Added to User");
+      res.redirect("/get");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.deleteExpense = (req, res, next) => {
+  const id = req.params.id;
+  console.log(id);
+  User.findByPk(id)
+    .then((user) => {
+      return user.destroy();
+    })
+    .then((result) => {
+      console.log("User Deleted");
+      res.redirect("/get");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
---
+++ expensenode2/routes/userRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+
+router.use(express.static("public"));
+
+
+router.get("/", userController.getappPage);
+
+router.get("/signup", userController.getloginPage);
+
+router.get("/users", userController.getUsers);
+
+router.get("/deleteUser/:id", userController.deleteExpense);
+
+router.post("/addUser", userController.addExpense);
+
+module.exports = router;
